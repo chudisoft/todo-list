eb901985ae0d6a38f5a4ec91b01f8fb605eb58a6
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-07-20 17:50:06 +0100

    Test for editing the task description. Test for updating an item's 'completed' status.

--- src/modules/todolist.test.js
+++ src/modules/todolist.test.js
@@ -7,12 +7,31 @@ const div = document.createElement('div');
 div.id = 'todo-list';
 document.body.appendChild(div);
 const todoNew = new TodoList();
+todoNew.renderTodoList();
 
 const todoTest = {
   description: `Todo ${todoNew.getTodoList().length + 1}`,
   completed: false,
   index: todoNew.getTodoList().length + 1,
 };
+const todoTestList = [
+  {
+    description: `Todo ${todoNew.getTodoList().length + 2}`,
+    completed: false,
+    index: todoNew.getTodoList().length + 2,
+  },
+  {
+    description: `Todo ${todoNew.getTodoList().length + 3}`,
+    completed: false,
+    index: todoNew.getTodoList().length + 3,
+  },
+  {
+    description: `Todo ${todoNew.getTodoList().length + 4}`,
+    completed: false,
+    index: todoNew.getTodoList().length + 4,
+  },
+];
+
 describe('CRUD operations', () => {
   test('Add todo', () => {
     const lastLength = todoNew.getTodoList().length;
@@ -20,9 +39,32 @@ describe('CRUD operations', () => {
     todoNew.saveData();
     expect(todoNew.getTodoList().length).toBe(lastLength + 1);
   });
-  test('Delete todo', () => {
-    const lastLength = todoNew.getTodoList().length;
-    todoNew.removeTodo(todoTest);
-    expect(todoNew.getTodoList().length).toBe(lastLength - 1);
+
+  test('Edit todo', () => {
+    const index = 0;
+    const newDescription = `Updated Todo ${index + 1}`;
+    const todoEdited = {
+      description: newDescription,
+      completed: true,
+      index: index + 1,
+    };
+    todoNew.updateTodo(todoEdited);
+    expect(todoNew.getTodoList()[0].description).toBe(newDescription);
+  });
+
+  test('Update completed todo', () => {
+    // Add 3 mock todos
+    todoTestList.forEach((x) => {
+      todoNew.addTodo(x, true);
+    });
+    const prevCompletedValue = todoNew.getTodoList()[1].completed;
+    // Mark as completed 2 todos
+    for (let index = 1; index < 2; index += 1) {
+      const element = todoNew.getTodoList()[index];
+      element.completed = true;
+      todoNew.updateTodo(element);
+    }
+
+    expect(todoNew.getTodoList()[1].completed).toBe(!prevCompletedValue);
   });
 });
